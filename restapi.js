3ebdb9e73f3c8d25b875d8ddbcd98d73528d4224
lizhizhou <lzz@meteroi.com>
2015-11-15 05:56:57 -0800

    Add restapi server

--- restapi.js
+++ restapi.js
@@ -1,1 +1,13 @@
 var fpga = require('./build/Release/openfpgaduino');
+var express = require('express');
+var app = express();
+
+var input_json;
+
+var output_json;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3333);
