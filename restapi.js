ec6d42096d35f5bc2d7dd0dcc58df37037851655
lizhizhou <lzz@meteroi.com>
2015-11-16 06:00:44 -0800

    add some restapi define

--- restapi.js
+++ restapi.js
@@ -1,13 +1,24 @@
-var fpga = require('./build/Release/openfpgaduino');
-var express = require('express');
 var app = express();
+var express = require('express');
 var swaggerTools = require('swagger-tools');
+var fpga = require('./build/Release/openfpgaduino');
 
 var input_json;
-
 var output_json;
 
 app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/fpga/device', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/fpga/memory', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/fpga/config', function (req, res) {
   res.send('Hello World!');
 });
 
