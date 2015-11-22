8ebf580a63005e3f03d40eb91ed0db8d7eca4fe1
lizhizhou <lzz@meteroi.com>
2015-11-22 08:30:52 +0800

    Restful api server

--- restapi.js
+++ restapi.js
@@ -1,16 +1,23 @@
-var app = express();
 var express = require('express');
-var swaggerTools = require('swagger-tools');
-var fpga = require('./build/Release/openfpgaduino');
-
+//var swaggerTools = require('swagger-tools');
+//var fpga = require('./build/Release/openfpgaduino');
+var app = express();
 var input_json;
 var output_json;
-
+var port = 3333;
 app.get('/', function (req, res) {
+  res.send('Hello this is the Openfpgaduino!');
+});
+
+app.post('/', function (req, res) {
+  res.send('Hello this is the Openfpgaduino!');
+});
+
+app.get('/fpga/device/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/fpga/device', function (req, res) {
+app.post('/fpga/device/', function (req, res) {
   res.send('Hello World!');
 });
 
@@ -18,8 +25,17 @@ app.get('/fpga/memory', function (req, res) {
   res.send('Hello World!');
 });
 
+app.post('/fpga/memory', function (req, res) {
+  res.send('Hello World!');
+});
+
 app.get('/fpga/config', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3333);
+app.post('/fpga/config', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(port);
+console.log("Restful API server on port", port)
--- unit_test.js
+++ unit_test.js
@@ -2,7 +2,5 @@ var fpga = require('./build/Release/openfpgaduino');
 console.log( 'This should be eight:', fpga.add(3,5));
 console.log( 'Sleep for 2 second');
 fpga.sleep(2);
-console.log( 'Set ioa');
-fpga.ioa(1,1);
 console.log( 'Set led');
 fpga.led(1,255,255,255);
