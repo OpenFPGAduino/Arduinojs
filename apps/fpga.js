ea37fd3321bbd32df1f5c695601346d4f80a4e06
lizhizhou <lzz@meteroi.com>
2016-01-08 12:11:52 +0800

    format the fpga.js

--- apps/fpga.js
+++ apps/fpga.js
@@ -1,43 +1,54 @@
-module.exports = function (app) {
+module.exports = function(app) {
     console.log('module fpga');
 
-//var fpga = require('././build/Release/openfpgaduino');
-var express    = require('express');        
-var router = express.Router();              
+    //var fpga = require('././build/Release/openfpgaduino');
+    var express = require('express');
+    var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our fpga api!' });   
-});
+    router.get('/', function(req, res) {
+        res.json({
+            message: 'hooray! welcome to our fpga api!'
+        });
+    });
 
-router.get('/device', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+    router.get('/device', function(req, res) {
+        res.json({
+            message: 'hooray! welcome to our api!'
+        });
+    });
 
-router.post('/device', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+    router.post('/device', function(req, res) {
+        res.json({
+            message: 'hooray! welcome to our api!'
+        });
+    });
 
 
-router.get('/memory', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+    router.get('/memory', function(req, res) {
+        res.json({
+            message: 'hooray! welcome to our api!'
+        });
+    });
 
-router.post('/memory', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+    router.post('/memory', function(req, res) {
+        res.json({
+            message: 'hooray! welcome to our api!'
+        });
+    });
 
-router.get('/config', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+    router.get('/config', function(req, res) {
+        res.json({
+            message: 'hooray! welcome to our api!'
+        });
+    });
 
-router.post('/config', function(req, res) {
-    console.log(req.body)
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+    router.post('/config', function(req, res) {
+        console.log(req.body)
+        res.json({
+            message: 'hooray! welcome to our api!'
+        });
+    });
 
-app.use('/fpga', router);
+    app.use('/fpga', router);
 
 }
-
-
-
