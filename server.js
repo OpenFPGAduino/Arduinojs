01df6c944ed06e49266e93fb263839e108a5779c
lizhizhou <lzz@meteroi.com>
2016-01-11 13:28:42 +0800

    add file upload support

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var loadDir    = require('./loaddir');
 var module     = loadDir('apps');
+var multer     = require('multer')
 
 var app        = express();                 // start express
 var logger     = log4js.getLogger();        // start logging
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // configure app to use /page to store static files
 app.use(express.static(__dirname + '/page'));
+// configure app to use /uploads to store upload files
+app.use(multer({ dest: './uploads/'}))
 
 var port = process.env.PORT || 8080;        // set our port
 
