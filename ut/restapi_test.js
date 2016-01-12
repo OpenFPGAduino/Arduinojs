562e6b29c497a3a00ace39fd0bc450d67f946a4c
lizhizhou <lzz@meteroi.com>
2016-01-12 18:27:06 +0800

    add unit test folder

---
+++ ut/restapi_test.js
@@ -0,0 +1,8 @@
+var http = require('http')
+
+http.get("http://localhost:8080/fpga/api/list", function(res) {
+  console.log("Got response: " + res.statusCode);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
