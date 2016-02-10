de15434524d7dc9713dac15bf1195728bc79aeff
lizhizhou <lzz@meteroi.com>
2016-02-10 16:37:15 +0800

    Add shelljs and ffl in linux.js

--- apps/linux.js
+++ apps/linux.js
@@ -1,3 +1,32 @@
 module.exports = function() {
     console.log('module linux');
+
+var ffi = require('ffi');
+
+var libm = ffi.Library('libm', {
+  'ceil': [ 'double', [ 'double' ] ]
+});
+console.log(libm.ceil(1.5)); // 2
+
+// You can also access just functions in the current process by passing a null
+var current = ffi.Library(null, {
+  'atoi': [ 'int', [ 'string' ] ]
+});
+console.log(current.atoi('1234')); // 1234
+
+require('shelljs/global');
+
+if (!which('git')) {
+  echo('Sorry, this script requires git');
+  exit(1);
+}
+
+
+ls('*.js').forEach(function(file) {
+  console.log(file)
+});
+
+
+
+
 }
