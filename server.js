9baa33d57fe323246f98200b6fbe43df04b2f8f9
lizhizhou <lzz@meteroi.com>
2016-03-01 01:43:55 +0800

    draft

--- server.js
+++ server.js
@@ -28,6 +28,31 @@ var tingodb = require('tingodb')();
 var optimist = require('optimist');
 var pjson = require('./package.json');
 var figlet = require('figlet');
+var mqtt    = require('mqtt');
+var mosca = require('mosca');
+
+
+var client  = mqtt.connect('http://test.mosquitto.org/'); 
+
+client.on('connect', function () {
+
+  client.subscribe('presence');
+
+  client.publish('presence', 'Hello mqtt');
+
+});
+
+ 
+
+client.on('message', function (topic, message) {
+
+  // message is Buffer 
+
+  console.log(message.toString());
+
+  client.end();
+
+});
 
 var app = express();                  // start express
 var logger = log4js.getLogger();      // start logging
