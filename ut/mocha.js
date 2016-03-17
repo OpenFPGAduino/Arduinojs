8f02cf51d0271361f1406a93234ce3eef8cc185b
lizhizhou <lzz@meteroi.com>
2016-03-17 13:19:25 +0800

    Add test for fpga main db

--- ut/mocha.js
+++ ut/mocha.js
@@ -1,6 +1,22 @@
 var assert = require('assert');
 describe('Angularjs', function() {
-  describe('test', function() {
+  describe('main', function() {
+    it('always true', function() {
+	assert(1)
+    });
+  });
+});
+
+describe('Angularjs', function() {
+  describe('fpga', function() {
+    it('always true', function() {
+	assert(1)
+    });
+  });
+});
+
+describe('Angularjs', function() {
+  describe('db', function() {
     it('always true', function() {
 	assert(1)
     });
