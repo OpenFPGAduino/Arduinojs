b612c682d5222a33d362065f9e6975a99dbbcf4e
lizhizhou <lzz@meteroi.com>
2016-03-17 22:24:35 +0800

    mocha  Integration test

--- ut/mocha.js
+++ ut/mocha.js
@@ -1,4 +1,15 @@
-var assert = require('assert');
+var assert = require('assert'); 
+var fork = require('child_process').fork;
+var child
+
+before(function() {
+    child = fork('./server',['--sim']);
+});
+
+beforeEach(function() {
+
+});
+
 describe('Angularjs', function() {
   describe('main', function() {
     it('always true', function() {
@@ -21,4 +32,12 @@ describe('Angularjs', function() {
 	assert(1)
     });
   });
+});
+
+afterEach(function() {
+
+});
+
+after(function() {
+    child.kill('SIGHUP');
 });
