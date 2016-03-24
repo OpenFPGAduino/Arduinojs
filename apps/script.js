e876680ae8d9e1d898f4fc271081d16ac84111f8
lizhizhou <lzz@meteroi.com>
2016-03-24 22:13:10 +0800

    Add get log test

--- apps/script.js
+++ apps/script.js
@@ -2,13 +2,6 @@ module.exports = function(app, logger, router) {
     logger.info('module script');
     var vm = require('vm');
 
-    router.get('/log', function(req, res) {
-        throw "error test";
-        res.json({
-            log: 'hooray! welcome to our api!'
-        });
-    });
-
     router.post('/run', function(req, res) {
         var script = req.body.script;
         //var runscript = vm.runInThisContext('localVar = 1;',
--- ut/mocha.js
+++ ut/mocha.js
@@ -1,13 +1,17 @@
-var assert = require('assert'); 
+var assert = require('assert');
 var fork = require('child_process').fork;
 var expect = require('chai').expect;
 var http = require('http');
-var request = require('request').defaults({baseUrl:"http://localhost:8080/"});;
+var request = require('request').defaults({
+    baseUrl: "http://localhost:8080/"
+});;
 var child
 
 before(function(done) {
-    child = fork('./server',['--sim']);
-    setTimeout(function(){done();},1900);
+    child = fork('./server', ['--sim']);
+    setTimeout(function() {
+        done();
+    }, 1900);
 });
 
 beforeEach(function() {
@@ -15,122 +19,149 @@ beforeEach(function() {
 });
 
 describe('Angularjs', function() {
-  describe('main', function() {
-	  it('get the front page', function(done) {
+    describe('main', function() {
+        it('get the front page', function(done) {
 
-		request('/', function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    console.log(body);
-		    done();
-		  }
-		  else {
-		   assert(0);
-		   done();
-		  }
-		});
-	  });
+            request('/', function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                    done();
+                } else {
+                    assert(0);
+                    done();
+                }
+            });
+        });
 
 
-    it('get the app list', function(done) {
-	 request("/list", function(error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    console.log(body);
-		    done();
-		  }
-		  else {
-		   assert(0);
-		   done();
-		  }
-		});
+        it('get the app list', function(done) {
+            request("/list", function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                    done();
+                } else {
+                    assert(0);
+                    done();
+                }
+            });
+        });
+        it('get the main app', function(done) {
+            request("/get/main.js", function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                } else {
+                    assert(0);
+
+                }
+                done();
+            });
+        });
+        it('load the main app', function(done) {
+            request.post({
+                headers: {
+                    "Connection": "close"
+                },
+                url: '/load',
+                method: 'POST',
+                json: true,
+                body: {
+                    filename: "main.js"
+                }
+            }, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                } else {
+                    assert(0);
+
+                }
+                done();
+            });
+        });
+
+        it('get the log', function(done) {
+            request.post({
+                headers: {
+                    "Connection": "close"
+                },
+                url: '/log',
+                method: 'POST',
+                json: true,
+                body: {
+                    length: 100,
+                    position: 0
+                }
+            }, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                } else {
+                    assert(0);
+
+                }
+                done();
+            });
+        });
+
     });
-    it('get the main app', function(done) {
-	 request("/get/main.js", function(error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    console.log(body);
-		  }
-		  else {
-		   assert(0);
-	
-		  }
-	   	  done();
-		});
-    });
-  it('get the main app', function(done) {
-	 request.post(headers: {"Connection": "close"},
-    url: '/load',
-    method: 'POST',
-    json:true,
-    body: {filename:"main.js"}function(error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    console.log(body);
-		  }
-		  else {
-		   assert(0);
-	
-		  }
-	   	  done();
-		});
-    });
-
-  });
 });
 
 describe('Angularjs', function() {
-  describe('fpga', function() {
-    it('always true', function() {
-	assert(1)
+    describe('fpga', function() {
+        it('always true', function() {
+            assert(1)
+        });
     });
-  });
 });
 
 describe('Angularjs', function() {
-  describe('db', function() {
-    it('add doc', function(done) {
-	 request.post({
-	headers: {"Connection": "close"},
-    url: '/db/add/test',
-    method: 'POST',
-    json:true,
-    body: {a:1,b:2}
-}, function(error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    console.log(body);
-		    done();
-		  }
-		  else {
-		   assert(0);
-		   done();
-		  }
-		});
+    describe('db', function() {
+        it('add doc', function(done) {
+            request.post({
+                headers: {
+                    "Connection": "close"
+                },
+                url: '/db/add/test',
+                method: 'POST',
+                json: true,
+                body: {
+                    a: 1,
+                    b: 2
+                }
+            }, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                    done();
+                } else {
+                    assert(0);
+                    done();
+                }
+            });
+        });
+        it('verfiy doc', function(done) {
+            request("/db/list/test", function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                    var data = JSON.parse(body)
+                    assert.equal(data[0].a, 1);
+                    assert.equal(data[0].b, 2);
+                    done();
+                } else {
+                    assert(0);
+                    done();
+                }
+            });
+        });
+        it('remove doc', function(done) {
+            request.del("/db/remove/test", function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                    done();
+                } else {
+                    assert(0);
+                    done();
+                }
+            });
+        });
     });
-    it('verfiy doc', function(done) {
-	 request("/db/list/test", function(error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    console.log(body);
-		    var data = JSON.parse(body)
-		    assert.equal(data[0].a ,1);
-		    assert.equal(data[0].b ,2);
-		    done();
-		  }
-		  else {
-		   assert(0);
-		   done();
-		  }
-		});
-    });
-    it('remove doc', function(done) {
-	 request.del("/db/remove/test", function(error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    console.log(body);
-		    done();
-		  }
-		  else {
-		   assert(0);
-		   done();
-		  }
-		});
-    });
-  });
 });
 
 afterEach(function() {
