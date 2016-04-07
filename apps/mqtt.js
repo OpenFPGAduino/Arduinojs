d9e23b80b13c4e53ff5ba49b286cf1115c61766b
lizhizhou <lzz@meteroi.com>
2016-04-07 18:58:51 +0800

    fix mqtt bug

--- apps/mqtt.js
+++ apps/mqtt.js
@@ -1,9 +1,7 @@
-module.exports = function(app, logger, io, db) {
+module.exports = function(app, logger, io, db, router) {
     console.log('module mqtt');
 
     var mqtt = require('mqtt');
-    var mosca = require('mosca');
-
 
     var client = mqtt.connect('http://test.mosquitto.org/');
 
@@ -32,19 +30,20 @@ module.exports = function(app, logger, io, db) {
 
     router.get('/server', function(req, res) {
 
-
+    var mosca = require('mosca');
+    	 
     var moscaSettings = {
         port: 1883, //mosca (mqtt) port
 
     };
 
-    //var server = new mosca.Server(moscaSettings); //here we start mosca
+    var server = new mosca.Server(moscaSettings); //here we start mosca
     server.on('ready', setup); //on init it fires up setup()
 
 
     // fired when the mqtt server is ready
     function setup() {
-        console.log('Mosca server is up and running')
+        console.log('Mosca server is up and running');
     }
 
         res.json({
