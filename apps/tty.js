6838f812181246be48ad2cdeafedf091b5005557
lizhizhou <lzz@meteroi.com>
2016-04-11 19:32:48 +0800

    bypass tty in simulation

--- apps/tty.js
+++ apps/tty.js
@@ -1,5 +1,10 @@
-module.exports = function(app, logger, router) {
+module.exports = function(app, logger, router, argv) {
     logger.info('module tty');
+
+    if (argv.sim) {
+        logger.debug("Skip fpga module for simulation");
+        return;
+    }    
 
     var tty = require('tty.js');
     var ttyserver = tty.createServer({
