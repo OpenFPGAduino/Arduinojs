2259b1c1963be0e5b408722322be50dadd525553
lizhizhou <lzz@meteroi.com>
2016-06-05 20:43:40 +0800

    Add linux tty mockup

--- ut/mock/linux.js
+++ ut/mock/linux.js
@@ -31,25 +31,13 @@ module.exports = function(app, logger, argv, router) {
         });
     });
 
+        router.post('/call', function(req, res) {
+   
 
-    router.post('/call', function(req, res) {
-        var ffi = require('ffi');
-
-        var libm = ffi.Library('libm', {
-            'ceil': ['double', ['double']]
+            res.json({
+                message: 'call linux lib!'
+            });
         });
-        console.log(libm.ceil(1.5)); // 2
-
-        // You can also access just functions in the current process by passing a null
-        var current = ffi.Library(null, {
-            'atoi': ['int', ['string']]
-        });
-        console.log(current.atoi('1234')); // 1234
-
-        res.json({
-            message: 'call linux lib!'
-        });
-    });
 
 
     router.get('/reboot', function(req, res) {
