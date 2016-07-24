9eb542d482948fb3c210dac41b8ae8c819ab1e03
lizhizhou <lzz@meteroi.com>
2016-07-24 19:00:41 +0800

    Add steering function

--- openfpgaduino.cc
+++ openfpgaduino.cc
@@ -177,6 +177,7 @@ DEFINE_FUNCTION_HANDLE(ain_a);
 DEFINE_FUNCTION_HANDLE(ain_b);
 DEFINE_FUNCTION_HANDLE(am2301_temperature);
 DEFINE_FUNCTION_HANDLE(am2301_moisture);
+DEFINE_FUNCTION_HANDLE(steering);
 DEFINE_FUNCTION_HANDLE(sleep);
 DEFINE_FUNCTION_HANDLE(usleep);
 
@@ -207,6 +208,7 @@ fun_table table[] = {
     DEFINE_FUNCTION(int,ain_b,(int id)),
     DEFINE_FUNCTION(float,am2301_temperature,(int id)),
     DEFINE_FUNCTION(float,am2301_moisture,(int id)),
+    DEFINE_FUNCTION(void,steering,(int id, int angle)),
     DEFINE_FUNCTION(unsigned int,sleep,(unsigned int seconds)),
     DEFINE_FUNCTION(int,usleep,(unsigned int useconds)),
     TABLE_NULL
