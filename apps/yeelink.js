a6d37c9bc1e13d4b09b20149021dd87a670d4619
lizhizhou <lzz@meteroi.com>
2016-10-24 12:38:28 +0800

    post get test ok

--- apps/yeelink.js
+++ apps/yeelink.js
@@ -1,8 +1,8 @@
 module.exports = function(app, logger, router, db, cron) {
     logger.info('module yeelink');
 
-var temp = '/v1.1/device/18329/sensor/327792/datapoints'
-var fan  = '/v1.1/device/18329/sensor/331103/datapoints'
+var temp = '/v1.1/device/18329/sensor/327792/datapoints/'
+var fan  = '/v1.1/device/18329/sensor/331103/datapoints/'
 
 var http = require("http");
     
@@ -10,7 +10,7 @@ function yeelink_post(path, postjson, callback) {
 var API_key = '954582e6b80f689de6d0a346c9c3d281';   
 postData = JSON.stringify(postjson);
 
-console.log(postData);
+logger.debug(postData);
 
 var options = {
   hostname: 'api.yeelink.net',
@@ -27,7 +27,7 @@ var options = {
 var req = http.request(options,callback);
 
 req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
+  logger.error('problem with request: ' + e.message);
 });
 
 // write data to request body
@@ -45,8 +45,6 @@ var options = {
   path: path,
   method: 'GET',
   headers: {
-    'Content-Type': 'application/json',
-    'Content-Length': postData.length,
     'U-ApiKey': API_key
   }
 };
@@ -54,11 +52,10 @@ var options = {
 var req = http.request(options,callback);
 
 req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
+  logger.error('problem with request: ' + e.message);
 });
 
 // write data to request body
-req.write(postData);
 req.end();
 
 }     
@@ -66,19 +63,27 @@ req.end();
 
 new cron('1 * * * * *', function() {
     logger.debug('You will see this message every second');
-var json = {}
     yeelink_post(temp,{
   "timestamp":new Date(),
   "value":26.5
 },  function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  logger.debug('STATUS: ' + res.statusCode);
+  logger.debug('HEADERS: ' + JSON.stringify(res.headers));
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
+    logger.debug('BODY: ' + chunk);
   });
 });
-    //yeelink_post(fan,JSON)
+   yeelink_get(fan, function(res) {
+  logger.debug('STATUS: ' + res.statusCode);
+  logger.debug('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    logger.debug('SWITCH: ' + chunk);
+    var json = JSON.parse(chunk)
+    logger.info("switch: "+ json.value)
+  });
+})
 }, null, true, 'America/Los_Angeles');
 
 
