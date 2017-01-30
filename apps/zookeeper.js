f70d9295275c38835f82f0c80261e7f8f2455f94
lizhizhou <lzz@meteroi.com>
2017-01-30 17:12:19 +0800

    update

--- apps/zookeeper.js
+++ apps/zookeeper.js
@@ -1,5 +1,6 @@
-module.exports = function(app, logger, io, db, router) {
+module.exports = function(app, logger, io, db, express) {
     logger.info('module zookeeper');
+    var router = express.Router();
     var zookeeper = require('node-zookeeper-client');
 
     app.use('/zookeeper', router);
