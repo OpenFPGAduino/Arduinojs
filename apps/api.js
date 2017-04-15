424aa24d102af68d859de567fce2a388bf532e88
lizhizhou <lzz@meteroi.com>
2017-04-15 22:09:37 +0800

    Add exit and restart

--- apps/api.js
+++ apps/api.js
@@ -21,6 +21,35 @@ module.exports = function(app, express, Set) {
         });
     });
 
+    router.get('/exit', function(req, res) {
+        res.json({
+            message: 'system exiting!'
+        });
+        setTimeout(process.exit(0), 1000);
+    });
+
+    router.post('/restart', function (req, res) {
+        var spawn = require('child_process').spawn;
+        (function main() {
+
+            if (process.env.process_restarting) {
+                delete process.env.process_restarting;
+                // Give old process one second to shut down before continuing ...
+                setTimeout(main, 1000);
+                return;
+            }
+
+            // Restart process ...
+            spawn(process.argv[0], process.argv.slice(1), {
+                env: { process_restarting: 1 },
+                stdio: 'ignore'
+            }).unref();
+        })();
+        res.json({
+            message: 'restart the nodejs'
+        });
+    });
+
     app.use('/api', router);
 
 }
