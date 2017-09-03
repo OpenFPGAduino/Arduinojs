dc96f790e410aa589b25c94b364f593cd2d2579d
lizhizhou <lzz@meteroi.com>
2017-09-03 17:39:48 +0800

    Add coap first version

--- apps/coap.js
+++ apps/coap.js
@@ -1,5 +1,27 @@
-module.exports = function(app, logger, io, db, express) {
+module.exports = function (app, logger, io, db, express) {
     logger.info('module coap');
     var router = express.Router();
+
+    var coap = require('coap')
+        , server = coap.createServer()
+
+    server.on('request', function(req, res) {
+    res.end('Hello ' + req.url.split('/')[1] + '\n')
+    })
+
+    // the default CoAP port is 5683
+    server.listen(function() {
+    var req = coap.request('coap://localhost/Matteo')
+
+    req.on('response', function(res) {
+        res.pipe(process.stdout)
+        res.on('end', function() {
+        process.exit(0)
+        })
+    })
+
+    req.end()
+    })
+    
     app.use('/coap', router);
 }
